e07207693dc9e79eccbfb7dd82b0517c8c1753ad
Ramon Soler Hernan <rsolerhernan@gmail.com>
2019-10-26 07:14:33 +0200

    added details to image and fix some problems

--- crown-clothing/src/components/menu-item/menu-item.component.jsx
+++ crown-clothing/src/components/menu-item/menu-item.component.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './menu-item.styles.scss';
 const MenuItem = ({title, imageUrl, size}) =>(
-    <div style={{
-        bakcgroundImage: `url(${imageUrl})`
-    }} className={`${size} menu-item`}>
+    <div className={`${size} menu-item`}>
+        <div className='background-image'
+            style={{
+                backgroundImage: `url(${imageUrl})`
+            }}
+        />
         <div className='content'>
             <h1 className='title'>
-            {title.toUppercase()}
+            {title.toUpperCase()}
             </h1>
             <span className='subtitle'>
             SHOP NOW
